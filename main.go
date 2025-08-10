9c6d939fbfbe62ba4b4722cb64110b053c19978d
lmihailovic <miha.luka1006@gmail.com>
2025-08-10 03:33:05 +0200

    parse html from url

--- main.go
+++ main.go
@@ -1,10 +1,32 @@
 package main
 
 import (
+	"fmt"
 	"github.com/lmihailovic/link/parse"
+	"golang.org/x/net/html"
+	"net/http"
+	"os"
 )
 
 func main() {
+	url := os.Args[1]
 
-	links := link.Parse()
+	resp, err := http.Get(url)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+	//bodyBytes, err := io.ReadAll(resp.Body)
+
+	body, err := html.Parse(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	links := link.Parse(body)
+
+	println("Links on page: " + url)
+	for k, v := range links {
+		fmt.Printf("\npath: %v\ntext: %v\n", k, v)
+	}
 }
