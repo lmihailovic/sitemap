2d707feadcbc0d40d7187fbd42cf4a6e36872681
lmihailovic <miha.luka1006@gmail.com>
2025-08-11 16:44:31 +0200

    function for getting links from a page

--- main.go
+++ main.go
@@ -6,27 +6,72 @@ import (
 	"golang.org/x/net/html"
 	"net/http"
 	"os"
+	"strings"
 )
+
+/*
+	SAMPLE SITEMAP XML
+
+	<?xml version="1.0" encoding="UTF-8"?>
+	<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+		<url>
+			<loc>http://www.example.com/</loc>
+		</url>
+		<url>
+			<loc>http://www.example.com/dogs</loc>
+		</url>
+	</urlset>
+*/
+
+func GetPageHtml(url string) (*html.Node, error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	body, err := html.Parse(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	return body, nil
+}
+
+func GetSitePages(url string) ([]string, error) {
+	body, err := GetPageHtml(url)
+	if err != nil {
+		return nil, err
+	}
+
+	pageLinks := link.Parse(body)
+	paths := make([]string, 0)
+
+	for path, _ := range pageLinks {
+		if strings.HasPrefix(path, "mailto") ||
+			strings.HasPrefix(path, "tel") ||
+			strings.HasPrefix(path, "#") {
+			continue
+		} else if strings.Contains(path, ".") {
+			continue
+		}
+		subUrl := url + path
+		paths = append(paths, subUrl)
+	}
+
+	return paths, nil
+}
 
 func main() {
 	url := os.Args[1]
 
-	resp, err := http.Get(url)
-	if err != nil {
-		panic(err)
-	}
-	defer resp.Body.Close()
-	//bodyBytes, err := io.ReadAll(resp.Body)
-
-	body, err := html.Parse(resp.Body)
+	pages, err := GetSitePages(url)
 	if err != nil {
 		panic(err)
 	}
 
-	links := link.Parse(body)
-
-	println("Links on page: " + url)
-	for k, v := range links {
-		fmt.Printf("\npath: %v\ntext: %v\n", k, v)
+	for _, page := range pages {
+		fmt.Println(page)
 	}
+
 }
